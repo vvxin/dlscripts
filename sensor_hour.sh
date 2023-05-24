d2143f152a22cdc57cd66e496d6a93d38f1107f7
WANG Xin <wangxin@gmail.com>
2023-05-24 18:19:45 +0800

    update hourly pic download to "1 hour ago"

--- sensor_hour.sh
+++ sensor_hour.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+#crontab:
+#32,42 * * * * ~/dlscripts/sensor_hour.sh >/dev/null 2>&1
+
 
 source ~/dlscripts/dl_func.sh
 
 (
 	cd ~/pic/Hourly
-	hourly_dl "$(date)" PM25
-	hourly_dl "$(date)" TSP
-	hourly_dl "$(date)" TVOC
-#	hourly_dl "$(date)" O3
-#	hourly_dl "$(date)" CO
-#	hourly_dl "$(date)" NO2
+	hourly_dl "$(date --date="1 hour ago")" PM25
+	hourly_dl "$(date --date="1 hour ago")" TSP
+	hourly_dl "$(date --date="1 hour ago")" TVOC
+#	hourly_dl "$(date --date="1 hour ago")" O3
+#	hourly_dl "$(date --date="1 hour ago")" CO
+#	hourly_dl "$(date --date="1 hour ago")" NO2
 )
 
