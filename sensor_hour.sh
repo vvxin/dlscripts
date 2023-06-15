2f39d35c5d145efb2cb6cd29b2a769551f2202e8
WANG Xin <wangxin@gmail.com>
2023-06-15 18:43:02 +0800

    add O3 hour map download

--- sensor_hour.sh
+++ sensor_hour.sh
@@ -10,7 +10,7 @@ source ~/dlscripts/dl_func.sh
 	hourly_dl "$(date --date="1 hour ago")" PM25
 	hourly_dl "$(date --date="1 hour ago")" TSP
 	hourly_dl "$(date --date="1 hour ago")" TVOC
-#	hourly_dl "$(date --date="1 hour ago")" O3
+	hourly_dl "$(date --date="1 hour ago")" O3
 #	hourly_dl "$(date --date="1 hour ago")" CO
 #	hourly_dl "$(date --date="1 hour ago")" NO2
 )
