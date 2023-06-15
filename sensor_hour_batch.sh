4941b4d901dac4b3f995f269f933fc3939a6f4de
WANG Xin <wangxin@gmail.com>
2023-06-15 18:52:59 +0800

    download O3 hour map

--- sensor_hour_batch.sh
+++ sensor_hour_batch.sh
@@ -5,18 +5,18 @@ source ~/dlscripts/dl_func.sh
 (
 	cd ~/pic/Hourly
 
-	start="2023-05-24 10:00"
-	end="2023-05-24 17:00"
+	start="2023-06-01 01:00"
+	end="2023-06-15 17:00"
 
 	start=`date -d "$start"`
 	end=`date -d "$end"`
 
 	while ((`date -d "$start" +%s` <= `date -d "$end" +%s`)); do
 		echo "$start"
-		hourly_dl "$start" PM25
-		hourly_dl "$start" TSP
-		hourly_dl "$start" TVOC
-#		hourly_dl "$start" O3
+#		hourly_dl "$start" PM25
+#		hourly_dl "$start" TSP
+#		hourly_dl "$start" TVOC
+		hourly_dl "$start" O3
 #		hourly_dl "$start" CO
 #		hourly_dl "$start" NO2
 		start=`date -d "$start +1 hour"`
